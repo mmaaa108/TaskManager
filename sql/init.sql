10fdf7d7b191e158f81da6e11650e182d2629d23
Mahmoud Alkhudari <116765162+mmaaa108@users.noreply.github.com>
2026-01-07 12:16:33 +0200

    Create init.sql

---
+++ sql/init.sql
@@ -0,0 +1,13 @@
+<?php
+$host = 'db'; // اسم الخدمة في Docker Compose
+$dbname = 'task_manager';
+$username = 'user';
+$password = 'password';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("خطأ في الاتصال بقاعدة البيانات: " . $e->getMessage());
+}
+?>
